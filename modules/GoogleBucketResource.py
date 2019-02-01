9dc8ffcd07ee7d6c92312eb45d16d9072cb502c2
Cinzia Malangone <cinzia@ebi.ac.uk>
2019-02-01 14:45:14 +0000

    Added evidences

--- modules/GoogleBucketResource.py
+++ modules/GoogleBucketResource.py
@@ -117,4 +117,32 @@ class GoogleBucketResource(object):
         blob.upload_from_filename(filename=original_filename)
 
 
+    def blob_metadata(bucket_name, blob_name):
+        """Prints out a blob's metadata."""
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.get_blob(blob_name)
 
+        print('Blob: {}'.format(blob.name))
+        print('Bucket: {}'.format(blob.bucket.name))
+        print('Storage class: {}'.format(blob.storage_class))
+        print('ID: {}'.format(blob.id))
+        print('Size: {} bytes'.format(blob.size))
+        print('Updated: {}'.format(blob.updated))
+        print('Generation: {}'.format(blob.generation))
+        print('Metageneration: {}'.format(blob.metageneration))
+        print('Etag: {}'.format(blob.etag))
+        print('Owner: {}'.format(blob.owner))
+        print('Component count: {}'.format(blob.component_count))
+        print('Crc32c: {}'.format(blob.crc32c))
+        print('md5_hash: {}'.format(blob.md5_hash))
+        print('Cache-control: {}'.format(blob.cache_control))
+        print('Content-type: {}'.format(blob.content_type))
+        print('Content-disposition: {}'.format(blob.content_disposition))
+        print('Content-encoding: {}'.format(blob.content_encoding))
+        print('Content-language: {}'.format(blob.content_language))
+        print('Metadata: {}'.format(blob.metadata))
+
+        if blob.retention_expiration_time:
+            print("retentionExpirationTime: {}"
+              .format(blob.retention_expiration_time))
