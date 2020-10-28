cac0f12930a94fc0be9384fa971856ae36c94cc8
Cinzia Malangone <cinzia.malangone@gmail.com>
2020-10-28 10:54:47 +0000

    Drug: change output directory

--- modules/Drug.py
+++ modules/Drug.py
@@ -1,3 +1,4 @@
+from definitions import PIS_OUTPUT_CHEMBL_API
 from .common import ElasticsearchReader
 from .common.ElasticsearchReader import ElasticsearchReader
 from datetime import datetime
@@ -61,7 +62,7 @@ class Drug(object):
             logger.debug("Validating elasticsearch port from config.")
             return port is not None and isinstance(port, int)
         indices = source['indices']
-        output_dir = source['output_location']
+        output_dir = PIS_OUTPUT_CHEMBL_API
         host = source['url']
         port = source['port']
 
