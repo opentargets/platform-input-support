6adb3891af83c918219cd0d46c12e19cc9876f45
Cinzia Malangone <cinzia.malangone@gmail.com>
2020-11-09 13:45:01 +0000

    convert owl to jsonld

--- modules/EFO.py
+++ modules/EFO.py
@@ -14,16 +14,16 @@ class EFO(object):
 
     def __init__(self, yaml):
         self.yaml = yaml
-        self.output_dir = PIS_OUTPUT_ANNOTATIONS
+        self.output_dir = yaml.gs_output_dir
         self.suffix = datetime.datetime.today().strftime('%Y-%m-%d')
-        self.gs_save_original_files_dir = yaml.gs_output_dir + '/efo_owl'
+        self.gs_save_json_dir = yaml.gs_output_dir + '/efo_json'
         self.list_files_downloaded = {}
         self.riot_cmd = shutil.which('riot')
         self.jq_cmd = shutil.which('jq')
 
 
     def riot(self, owl_file, json_file, owl_jq):
-        jsonwrite = open(PIS_OUTPUT_ANNOTATIONS + '/' + json_file, "wb")
+        jsonwrite = open(PIS_OUTPUT_EFO + '/' + json_file, "wb")
         try:
             riotp = subprocess.Popen([self.riot_cmd, "--output", "JSON-LD", owl_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
             jqp = subprocess.Popen(["jq", "-r", owl_jq], stdin=riotp.stdout, stdout=subprocess.PIPE)
@@ -48,20 +48,21 @@ class EFO(object):
 
     def download_owl(self, yaml_info):
         for entry in yaml_info:
-            download = DownloadResource(PIS_OUTPUT_EFO)
+            download = DownloadResource(PIS_OUTPUT_ANNOTATIONS)
             destination_filename = download.execute_download(entry)
             self.list_files_downloaded[destination_filename] = {'resource': entry.resource,
-                                                                'gs_output_dir': self.gs_save_original_files_dir}
+                                                                'gs_output_dir': self.output_dir}
             json_filename = self.convert_owl_to_jsonld(destination_filename, entry.owl_jq)
-            self.list_files_downloaded[json_filename] = {'resource': entry.resource,
-                                                            'gs_output_dir': self.gs_output_dir}
+            self.list_files_downloaded[json_filename] = {'resource': None,
+                                                            'gs_output_dir': self.gs_save_json_dir}
 
 
     def generate_efo(self):
-        logger.info("EFO process")
+        logger.info("===> EFO-ECO: riot path: "+ self.riot_cmd)
+        logger.info("===> EFO-ECO: JQ path: " + self.jq_cmd)
         self.download_owl(self.yaml.disease_phenotypes_downloads)
         self.download_owl(self.yaml.efo_downloads)
-
+        self.download_owl(self.yaml.eco_downloads)
 
         return self.list_files_downloaded
 
