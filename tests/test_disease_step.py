0a05eaa1b3203a9ea99010bdfa3087b5758af2b7
Cinzia <cinzia.malangone@gmail.com>
2021-03-24 16:39:47 +0000

    Final draft

---
+++ tests/test_disease_step.py
@@ -0,0 +1,23 @@
+import unittest
+from definitions import ROOT_DIR
+
+from modules.helpers.HPO import HPO
+
+class TestDiseaseStep(unittest.TestCase):
+    """
+    TBC: To be complete
+    """
+
+    def setUp(self):
+        self.hpo_filename = "resources/hp.jsonl"
+
+    # exclude obsolete term
+    def testHPO(self):
+        HPOModule = HPO(self.hpo_filename)
+        HPOModule.generate()
+        assert len(HPOModule.hpo) == 4
+
+
+
+
+
