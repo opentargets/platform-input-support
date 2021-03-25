0f7786e9f152e7431304003fa8853bce9e38ff11
Cinzia <cinzia.malangone@gmail.com>
2021-03-25 14:00:51 +0000

    Improve test

--- tests/test_disease_step.py
+++ tests/test_disease_step.py
@@ -1,7 +1,9 @@
 import unittest
 from definitions import ROOT_DIR
+import subprocess
 
 from modules.helpers.HPO import HPO
+from modules.helpers.EFO import EFO
 
 class TestDiseaseStep(unittest.TestCase):
     """
@@ -10,12 +12,29 @@ class TestDiseaseStep(unittest.TestCase):
 
     def setUp(self):
         self.hpo_filename = ROOT_DIR+"/tests/resources/hp.jsonl"
+        self.efo_filename = ROOT_DIR+"/tests/resources/efo.jsonl"
+
+
+    def testDisease(self):
+        EFOModule = EFO(self.efo_filename)
+        process = subprocess.Popen("cat "+ self.efo_filename +"| grep -v deprecated | wc -l",
+                                   shell=True,
+                                   stdout=subprocess.PIPE,
+                                   )
+        num_valid_EFO = int(process.communicate()[0].decode("utf-8").replace("\n",""))
+        EFOModule.generate()
+        assert len(EFOModule.diseases) == num_valid_EFO
 
     # exclude obsolete term
     def testHPO(self):
         HPOModule = HPO(self.hpo_filename)
+        process = subprocess.Popen("cat "+ self.hpo_filename +"| grep -v deprecated | wc -l",
+                                   shell=True,
+                                   stdout=subprocess.PIPE,
+                                   )
+        num_valid_HPO = int(process.communicate()[0].decode("utf-8").replace("\n", ""))
         HPOModule.generate()
-        assert len(HPOModule.hpo) == 4
+        assert len(HPOModule.hpo) == num_valid_HPO
 
 
 
