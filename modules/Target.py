16f5b11fa3c7b621de3443446905e851c15a74a0
Jarrod Baker <jarrod.bakeriii@gmail.com>
2021-03-30 10:23:01 +0100

    Add Gene Ontology input files to Target step.

--- modules/Target.py
+++ modules/Target.py
@@ -7,14 +7,6 @@ from definitions import PIS_OUTPUT_TARGET
 logger = logging.getLogger(__name__)
 
 
-class TargetGo(object):
-    def __init__(self):
-        pass
-
-    def execute(self):
-        pass
-
-
 class Target(object):
     """
     Retrieve resources required for ETL Target step from Ensembl FTP.
@@ -26,10 +18,10 @@ class Target(object):
         self.output_dir = PIS_OUTPUT_TARGET
 
         # ensembl ftp settings
-        self.url = self.ensembl_config.url
+        self.ensembl_ftp_url = self.ensembl_config.ensembl_ftp_url
         self.path = self.ensembl_config.path.replace('{suffix}', str(self.ensembl_config.release))
 
-    def download_ftp(self, ftp_path: str, file: str, out_file_name: str) -> str:
+    def download_ftp(self, ftp_url: str, ftp_path: str, file: str, out_file_name: str) -> str:
         """
         download remote file to output_file_name. output_file_name is absolute path and name.
         """
@@ -37,7 +29,7 @@ class Target(object):
             logger.info(f"Found {out_file_name}: will not download again.")
             pass
         else:
-            ftp = FTP(self.url)
+            ftp = FTP(ftp_url)
             ftp.login()
             ftp.cwd(ftp_path)
             with open(out_file_name, 'wb') as outfile:
@@ -45,18 +37,39 @@ class Target(object):
                 ftp.retrbinary('RETR ' + file, outfile.write)
                 return out_file_name
 
+    def download_go(self) -> List[str]:
+        logger.info("Downloading gene ontology files.")
+        ftp_url = self.config.go.url
+        ftp_path_human = self.config.go.goa_human.path
+        ftp_path_ensembl = self.config.go.goa_ensembl.path
+        go_output_dir = self.output_dir + "/go/"
+
+        # download human files
+        files_to_download: List[str] = self.config.go.goa_human.files
+        downloaded = []
+        for f in files_to_download:
+            output_location = go_output_dir + f
+            downloaded.append(self.download_ftp(ftp_url, ftp_path_human, f, output_location))
+
+        # download ensembl file
+        downloaded.append(self.download_ftp(ftp_url,
+                                            ftp_path_ensembl,
+                                            self.config.go.goa_ensembl.filename,
+                                            go_output_dir + self.config.go.goa_ensembl.filename))
+        return downloaded
+
     def download_homo_sapiens(self) -> str:
         logger.info("Downloading ensembl homo_sapiens file.")
         ftp_path = self.path + self.ensembl_config.homo_sapiens.path
         file_to_download: str = self.ensembl_config.homo_sapiens.filename
         output_location: str = self.output_dir + "/ensembl/" + self.ensembl_config.homo_sapiens.filename
-        return self.download_ftp(ftp_path, file_to_download, output_location)
+        return self.download_ftp(self.ensembl_ftp_url, ftp_path, file_to_download, output_location)
 
     def download_species(self) -> str:
         logger.info("Downloading target species file.")
         file_to_download = self.ensembl_config.species
         output_location = self.output_dir + "/ensembl/" + self.ensembl_config.species
-        return self.download_ftp(self.path, file_to_download, output_location)
+        return self.download_ftp(self.ensembl_ftp_url, self.path, file_to_download, output_location)
 
     def download_ortholog(self) -> List[str]:
         logger.info("Downloading ortholog files.")
@@ -66,7 +79,7 @@ class Target(object):
         for f in files_to_download:
             fn = f.replace("{suffix}", str(self.ensembl_config.release))
             output_location = self.output_dir + "/ensembl/" + fn
-            downloaded.append(self.download_ftp(ftp_path, fn, output_location))
+            downloaded.append(self.download_ftp(self.ensembl_ftp_url, ftp_path, fn, output_location))
         return downloaded
 
     def create_output_dirs(self):
@@ -90,6 +103,7 @@ class Target(object):
         sources: List[str] = [self.download_species(),
                               self.download_homo_sapiens()]
         sources = sources + self.download_ortholog()
+        sources = sources + self.download_go()
         downloaded_files = {}
         for f in sources:
             downloaded_files[f] = {'resource': "{}".format(f), 'gs_output_dir': self.config[
