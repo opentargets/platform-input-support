73301b6ec3083fde6b141f8bc7a4aa414255a65f
Cinzia <cinzia.malangone@gmail.com>
2021-05-10 15:21:52 +0100

    EBI infrastructure parameter svc account

--- Dockerfile
+++ Dockerfile
@@ -8,13 +8,8 @@ RUN conda update -n base -c defaults conda
 RUN conda env create --file environment.yaml
 ENV PATH /opt/conda/envs/$conda_env/bin:$PATH
 
-# Make RUN commands use the new environment:
-#SHELL ["conda", "run", "-n", "pis-py3", "/bin/bash", "-c"]
-
 #put the application in the right place
 WORKDIR /usr/src/app
 COPY . /usr/src/app
-
-#RUN echo "PWD is: $PWD"
 
 ENTRYPOINT [ "docker-script/entrypoint.sh" ]
--- singularity/ebi.sh
+++ singularity/ebi.sh
@@ -3,14 +3,19 @@
 # $1 branch or tag docker image
 # $2 steps to run
 # $3 storage path : not mandatory
-# Eg. ebi.sh master drug ot-snapshot/21.04
-# Eg. ebi.sh 21.04 drug ot-snapshot/21.04/input
+# $4 path to svc account
+
+# Eg. ebi.sh master drug
+# Eg. ebi.sh 21.04 drug ot-snapshots/21.04/input /path/credential.json
+
 if [ -z "$1" ]
   then
     echo "No argument supplied"
   else
 echo "Running on noah using lsf"
+ssh noah-login "cat > open-targets-pis.json" < $4
 ssh noah-login <<EOF_A
+ls -la /homes/$USER/open-targets-pis.json
 cd /nfs/ftp/private/otftpuser/output_pis
 cat > singularity_cmd.sh <<EOF_B
   cd /nfs/ftp/private/otftpuser/output_pis
@@ -19,7 +24,7 @@ cat > singularity_cmd.sh <<EOF_B
   -B /nfs/ftp/private/otftpuser/output_pis:/usr/src/app/output \\
   docker://quay.io/opentargets/platform-input-support:$1 \\
   conda run --no-capture-output -n pis-py3 python3 /usr/src/app/platform-input-support.py \\
-  -gkey /homes/$USER/.config/gcloud/application_default_credentials.json \\
+  -gkey /homes/$USER/open-targets-pis.json \\
   -gb $3 -steps $2
 
   echo "Done" > /nfs/ftp/private/otftpuser/output_pis/lsf_done
@@ -28,4 +33,5 @@ cat > singularity_cmd.sh <<EOF_B
 EOF_B
 cat singularity_cmd.sh | bsub
 EOF_A
+
 fi
