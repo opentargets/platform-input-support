5b1cd7fead25399ed76c9269dac24b2a60744500
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-08-31 23:52:00 +0100

    OpenFDA module, heading

---
+++ modules/OpenFDA.py
@@ -0,0 +1,7 @@
+"""
+This module gathers all data related to OpenFDA FAERS database, with a focus on Drug Events.
+
+In addition, a collection of blacklisted events is downloaded for later use in the ETL backend pipeline.
+"""
+
+# TODO
