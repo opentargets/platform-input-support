624a2bf5393c7bd0a2fec83c1cfbed83fa3e6001
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-08-31 23:54:37 +0100

    OpenFDA module, import needed components

--- modules/OpenFDA.py
+++ modules/OpenFDA.py
@@ -4,4 +4,12 @@ This module gathers all data related to OpenFDA FAERS database, with a focus on 
 In addition, a collection of blacklisted events is downloaded for later use in the ETL backend pipeline.
 """
 
-# TODO
+import os
+import zipfile
+import logging
+import warnings
+
+from definitions import PIS_OUTPUT_OPENFDA
+from .DownloadResource import DownloadResource
+from datetime import datetime
+
