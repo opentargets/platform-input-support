55aea1794fbd591e656b31f1963c3647c1a569ed
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-01-24 13:08:01 +0000

    execute_download() pydoc and refactoring

--- modules/common/DownloadResource.py
+++ modules/common/DownloadResource.py
@@ -7,7 +7,10 @@ import urllib.request, urllib.parse, urllib.error
 # Common packages
 from typing import Dict
 from modules.common.TqdmUpTo import TqdmUpTo
+
 from pip._internal.utils.deprecation import deprecated
+
+logger = logging.getLogger(__name__)
 
 
 # Decorator for the threading parameter.
@@ -22,15 +25,17 @@ def threaded(fn):
     return wrapper
 
 
-logger = logging.getLogger(__name__)
-
-
 class DownloadResource(object):
     """
     This class implements a URI download helper
     """
 
     def __init__(self, output_dir):
+        """
+        Constructor
+
+        :param output_dir path to download destination folder
+        """
         self.suffix = datetime.datetime.today().strftime('%Y-%m-%d')
         self.output_dir = output_dir
 
@@ -40,42 +45,50 @@ class DownloadResource(object):
 
     def set_filename(self, filename) -> str:
         """
-        Build final file path and file name including suffix
-        @param filename Base file name
+        Build final destination file path
+
+        :param filename: Base file name
         """
         return os.path.join(self.output_dir, filename.replace('{suffix}', self.suffix))
 
     def execute_download(self, resource_info, retry_count=1) -> str:
         """
         Perform downloading of a resource described by the given resource information.
-        @param resource_info information on the resource to download
-        @param retry_count number of times to re-try downloading the resource in case of error
+
+        :param resource_info: information on the resource to download
+        :param retry_count: number of times to re-try downloading the resource in case of error, '1' by default
+        :return: the download destination path when successful, empty path if not
         """
         logger.debug("Start to download\n\t{uri} ".format(uri=resource_info.uri))
-        try:
-            opener = urllib.request.build_opener()
-            opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-            if resource_info.accept:
-                opener.addheaders = [('User-agent', 'Mozilla/5.0'), ('Accept', resource_info.accept)]
-            urllib.request.install_opener(opener)
-            destination_filename = self.set_filename(resource_info.output_filename)
-            with TqdmUpTo(unit='B', unit_scale=True, miniters=1,
-                          desc=resource_info.uri.split('/')[-1]) as t:  # all optional kwargs
-                urllib.request.urlretrieve(resource_info.uri, destination_filename,
-                                           reporthook=t.update_to, data=None)
-            return destination_filename
-        except urllib.error.URLError as e:
-            logger.error('Download error:', e.reason)
-            if retry_count > 0:
-                if hasattr(e, 'code') and 500 <= e.code < 600:
-                    return self.execute_download(resource_info, retry_count - 1)
-        except IOError as io_error:
-            logger.error("IOError: {io_error}".format(io_error=io_error))
-            return None
-        except Exception as e:
-            logger.error("Error: {msg}".format(msg=e))
-            return None
+        for attempt in range(retry_count):
+            try:
+                opener = urllib.request.build_opener()
+                opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+                if resource_info.accept:
+                    opener.addheaders = [('User-agent', 'Mozilla/5.0'), ('Accept', resource_info.accept)]
+                urllib.request.install_opener(opener)
+                destination_filename = self.set_filename(resource_info.output_filename)
+                with TqdmUpTo(unit='B', unit_scale=True, miniters=1,
+                              desc=resource_info.uri.split('/')[-1]) as t:  # all optional kwargs
+                    urllib.request.urlretrieve(resource_info.uri, destination_filename,
+                                               reporthook=t.update_to, data=None)
+                return destination_filename
+            except urllib.error.URLError as e:
+                logger.error('Download error:', e.reason)
+                try:
+                    if 500 <= e.code < 600:
+                        continue
+                    break
+                except AttributeError:
+                    pass
+            except IOError as io_error:
+                logger.error("IOError: {}".format(io_error))
+            except Exception as e:
+                logger.error("Error: {}".format(e))
+            break
+        return ''
 
+    @deprecated(reason='no longer used, it would benefit from a refactoring round')
     @threaded
     def execute_download_threaded(self, resource_info):
         self.execute_download(resource_info)
