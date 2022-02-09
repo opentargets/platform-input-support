fe090654609cedb4ade11715b2348e402ec49b62
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-02-09 01:11:04 +0000

    pipeline mouse phenotype data collection step

--- plugins/MousePhenotypes.py
+++ plugins/MousePhenotypes.py
@@ -1,15 +1,27 @@
 import logging
 from yapsy.IPlugin import IPlugin
 from modules.common.Downloads import Downloads
+
 logger = logging.getLogger(__name__)
 
-"""
 
-"""
 class MousePhenotypes(IPlugin):
+    """
+    Mouse Phenotypes pipeline step
+    """
     def __init__(self):
+        """
+        Constructor, prepare logging subsystem
+        """
         self._logger = logging.getLogger(__name__)
 
-    def process(self, conf, output, cmd_conf):
+    def process(self, conf, output, cmd_conf=None):
+        """
+        Pipeline step implementation
+
+        :param conf: step configuration object
+        :param output: destination information for the results of this step
+        :param cmd_conf: NOT USED
+        """
         self._logger.info("MousePhenotypes step")
         Downloads(output.prod_dir).exec(conf)
