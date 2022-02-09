9248578d5070bdf28494a22036abb4487f3e1682
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-02-09 01:08:38 +0000

    pipeline interaction step pydoc

--- plugins/Interactions.py
+++ plugins/Interactions.py
@@ -1,15 +1,27 @@
 import logging
 from yapsy.IPlugin import IPlugin
 from modules.common.Downloads import Downloads
+
 logger = logging.getLogger(__name__)
 
-"""
 
-"""
 class Interactions(IPlugin):
+    """
+    Interaction data collection step
+    """
     def __init__(self):
+        """
+        Constructor, prepare logging subsystem
+        """
         self._logger = logging.getLogger(__name__)
 
-    def process(self, conf, output, cmd_conf):
+    def process(self, conf, output, cmd_conf=None):
+        """
+        Interaction pipeline step implementation
+
+        :param conf: step configuration object
+        :param output: output information object on where the results of this step should be placed
+        :param cmd_conf: NOT USED
+        """
         self._logger.info("Interactions step")
         Downloads(output.prod_dir).exec(conf)
