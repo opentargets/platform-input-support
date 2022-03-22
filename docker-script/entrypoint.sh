42b9ebaba94c280db3ddb30475d75982ebf3a2ab
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-03-22 21:41:52 +0000

    activate the given service account, ignore the error if none given

--- docker-script/entrypoint.sh
+++ docker-script/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-echo "Running Platform input support with parameters $*"
+echo "[GCP] Activate the provided service account"
+gcloud auth activate-service-account --key-file=/srv/credentials/open-targets-gac.json
+
+echo "[PIS] Running Platform input support with parameters $*"
 conda run --no-capture-output -n pis-py3.8 python3 platform-input-support.py "$@"
 
 exit $!
