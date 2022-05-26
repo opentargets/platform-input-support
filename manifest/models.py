445f489d226142d4e578f7f2a5a8354cd2fb99d8
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-05-26 12:31:20 +0100

    refactory of baseline manifest models

--- manifest/models.py
+++ manifest/models.py
@@ -1,9 +1,32 @@
 # Manifest file Models
+import time
+import typing
 
 # TODO - Global Data model
-class Manifest:
+class ManifestDocument:
     def __init__(self):
-        pass
+        """
+        Constructor for the Manifest file root content
+        """
+        timestamp :float = time.time()
+        self.created :float = timestamp
+        self.modified :float = timestamp
+        self.steps :typing.Dict[str, ManifestStep] = dict()
 
 # TODO - Step Data model
+class ManifestStep:
+    def __init__(self):
+        timestamp :float = time.time()
+        self.created :float = timestamp
+        self.modified :float = timestamp
+        self.steps = typing.Dict[str, ManifestResource]
+
 # TODO - Resource data model
+class ManifestResource:
+    def __init__(self):
+        timestamp :float = time.time()
+        self.created :float = timestamp
+        self.modified :float = timestamp
+        self.source_url :str = "NOT SPECIFIED"
+        self.path_destination :str = "NOT SPECIFIED"
+        self.checksum :str = "NOT SPECIFIED"
