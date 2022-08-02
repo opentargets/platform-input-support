ee48d501098c13fcc27559233ce4df93c7def716
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-08-02 12:32:06 +0200

    plugin heading and skeleton

--- plugins/OtarProjects.py
+++ plugins/OtarProjects.py
@@ -3,4 +3,15 @@ from yapsy.IPlugin import IPlugin
 
 logger = logging.getLogger(__name__)
 
-# TODO
+"""
+This module gathers all data related to OTAR Projects (metadata and mapping)
+"""
+
+
+class OtarProjects(IPlugin):
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def process(self, conf, output, cmd_conf=None):
+        # TODO
+        pass
