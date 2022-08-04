2c0c5e14aaf2160b31c3275006caf1bd00650fb2
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-08-04 11:32:56 +0200

    OTAR projects information collection step

--- plugins/Otar.py
+++ plugins/Otar.py
@@ -1,5 +1,9 @@
+import os
 import logging
+
 from yapsy.IPlugin import IPlugin
+from modules.common import create_folder
+from modules.common.GoogleSpreadSheet import get_spreadsheet_handler
 
 logger = logging.getLogger(__name__)
 
@@ -14,3 +18,17 @@ class Otar(IPlugin):
 
     def process(self, conf, output, cmd_conf=None):
         self.logger.info("OTAR Projects data collection - START -")
+        gcp_credentials = conf.google_credential_key
+        dst_folder = os.path.join(output.prod_dir, conf.gs_output_dir)
+        self.logger.debug("Prepare destination folder at '{}'".format(dst_folder))
+        create_folder(dst_folder)
+        if gcp_credentials is None:
+            self.logger.warning("NO GCP credentials have been provided")
+        for sheet in conf.sheets:
+            path_dst = os.path.join(dst_folder, sheet.output_filename)
+            handler = get_spreadsheet_handler(sheet.id_spreadsheet,
+                                              sheet.worksheet_name,
+                                              path_dst,
+                                              sheet.output_format,
+                                              gcp_credentials)
+            handler.download()
