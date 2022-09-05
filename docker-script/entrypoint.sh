5906f41c2ce5c996c9283d137fbdd2c7163a8b40
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-09-05 22:11:59 +0100

    update docker container entry point script with new conda environment name

--- docker-script/entrypoint.sh
+++ docker-script/entrypoint.sh
@@ -4,6 +4,6 @@ echo "[GCP] Activate the provided service account"
 gcloud auth activate-service-account --key-file=/srv/credentials/open-targets-gac.json
 
 echo "[PIS] Running Platform input support with parameters $*"
-conda run --no-capture-output -n pis-py3.8 python3 platform-input-support.py "$@"
+conda run --no-capture-output -n otpis python3 platform-input-support.py "$@"
 
 exit $!
