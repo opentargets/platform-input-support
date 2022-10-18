7196051bfc290eef7e822f05d62588d60a8cd9aa
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-10-18 14:43:42 +0100

    warning on missing steps

--- modules/RetrieveResource.py
+++ modules/RetrieveResource.py
@@ -63,7 +63,7 @@ class RetrieveResource(object):
                 matching_plugins.append(plugin)
                 lowercase_steps.remove(plugin.lower())
 
-        logger.info("Steps not found:\n" + ','.join(lowercase_steps))
+        logger.warning("Steps not found:\n" + ','.join(lowercase_steps))
         return matching_plugins
 
     def steps(self):
