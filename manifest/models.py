2eca3f781e241b3d07fe2bd6b4f5114a4629d380
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-11-09 15:51:33 +0000

    update the model for ISO8601 based timestamps

--- manifest/models.py
+++ manifest/models.py
@@ -1,38 +1,38 @@
 # Manifest file Models
-import time
 import typing
 import logging
+from modules.common.TimeUtils import get_timestamp_iso_utc_now
 
 # Logging
 logger = logging.getLogger(__name__)
 
 # TODO - Global Data model
-class ManifestDocument:
-    def __init__(self):
+class ManifestDocument(object):
+    def __init__(self, timestamp :str =None):
         """
         Constructor for the Manifest file root content
         """
-        timestamp :float = time.time()
-        self.session :float = timestamp
-        self.created :float = timestamp
-        self.modified :float = timestamp
+        if timestamp is None:
+            timestamp = get_timestamp_iso_utc_now()
+        self.session :str = timestamp
+        self.created :str = timestamp
+        self.modified :str = timestamp
         self.steps :typing.Dict[str, ManifestStep] = dict()
 
 # TODO - Step Data model
 class ManifestStep:
     def __init__(self):
-        timestamp :float = time.time()
-        self.created :float = timestamp
-        self.modified :float = timestamp
-        self.step :str = "NOT SPECIFIED"
-        self.steps :typing.Dict[str, ManifestResource] = dict()
+        timestamp :str = get_timestamp_iso_utc_now()
+        self.created :str = timestamp
+        self.modified :str = timestamp
+        self.step :str = "NONAME"
 
 # TODO - Resource data model
 class ManifestResource:
     def __init__(self):
-        timestamp :float = time.time()
-        self.created :float = timestamp
-        self.modified :float = timestamp
+        timestamp :str = get_timestamp_iso_utc_now()
+        self.created :str = timestamp
+        self.modified :str = timestamp
         self.source_url :str = "NOT SPECIFIED"
         self.path_destination :str = "NOT SPECIFIED"
         self.checksum :str = "NOT SPECIFIED"
