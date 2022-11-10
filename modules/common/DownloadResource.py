eadbc1b78c31bea19483a3628356b016665c701f
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-11-10 14:42:24 +0000

    handle downloaded resource information via manifest data model for HTTP downloads

--- modules/common/DownloadResource.py
+++ modules/common/DownloadResource.py
@@ -4,6 +4,7 @@ import datetime
 import threading
 import subprocess
 import urllib.request, urllib.parse, urllib.error
+from manifest import ManifestResource, ManifestStatus, get_manifest_service
 # Common packages
 from typing import Dict
 
@@ -48,7 +49,7 @@ class DownloadResource(object):
         """
         return os.path.join(self.output_dir, filename.replace('{suffix}', self.suffix))
 
-    def execute_download(self, resource_info, retry_count=1) -> str:
+    def execute_download(self, resource_info, retry_count=1) -> ManifestResource:
         """
         Perform downloading of a resource described by the given resource information.
 
@@ -56,7 +57,11 @@ class DownloadResource(object):
         :param retry_count: number of times to re-try downloading the resource in case of error, '1' by default
         :return: the download destination path when successful, empty path if not
         """
+        # TODO - Change in return type breaks the unit test, although the return value wasn't used anywhere but in tests
         logger.debug("Start to download\n\t{} ".format(resource_info.uri))
+        downloaded_resource = get_manifest_service().new_resource()
+        downloaded_resource.source_url = resource_info.uri
+        errors = list()
         for attempt in range(retry_count):
             try:
                 opener = urllib.request.build_opener()
@@ -65,12 +70,12 @@ class DownloadResource(object):
                     opener.addheaders = [('User-agent', 'Mozilla/5.0'), ('Accept', resource_info.accept)]
                 urllib.request.install_opener(opener)
                 destination_filename = self.set_filename(resource_info.output_filename)
+                downloaded_resource.path_destination = destination_filename
                 logger.info(f"[DOWNLOAD] BEGIN: '{resource_info.uri}' -> '{destination_filename}'")
                 urllib.request.urlretrieve(resource_info.uri, destination_filename)
-                logger.info(f"[DOWNLOAD] END: '{resource_info.uri}' -> '{destination_filename}'")
-                return destination_filename
             except urllib.error.URLError as e:
-                logger.error('[DOWNLOAD] ERROR:', e.reason)
+                errors.append(f"[DOWNLOAD] ERROR: {e.reason}")
+                logger.error(errors[-1])
                 try:
                     # TODO - This is useful information to report back to the caller
                     if 500 <= e.code < 600:
@@ -79,11 +84,19 @@ class DownloadResource(object):
                 except AttributeError:
                     pass
             except IOError as io_error:
-                logger.error("IOError: {}".format(io_error))
+                errors.append("IOError: {}".format(io_error))
+                logger.error(errors[-1])
             except Exception as e:
-                logger.error("Error: {}".format(e))
-            break
-        return ''
+                errors.append("Error: {}".format(e))
+                logger.error(errors[-1])
+            else:
+                downloaded_resource.status_completion = ManifestStatus.COMPLETED
+                downloaded_resource.msg_completion = "At least one download attempt finished with no errors"
+                logger.info(f"[DOWNLOAD] END: '{resource_info.uri}' -> '{destination_filename}'")
+        if downloaded_resource.status_completion == ManifestStatus.NOT_COMPLETED:
+            downloaded_resource.status_completion = ManifestStatus.FAILED
+            downloaded_resource.msg_completion = " -E- ".join(errors)
+        return downloaded_resource
 
     # @deprecated(reason='no longer used, it would benefit from a refactoring round')
     @threaded
