df2606785702d348fa8ad5332ff2d5d33ade6e42
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-11-10 15:33:57 +0000

    GO step reporting via manifest subsystem

--- plugins/GO.py
+++ plugins/GO.py
@@ -1,6 +1,7 @@
 import logging
 from yapsy.IPlugin import IPlugin
 from modules.common.Downloads import Downloads
+from manifest import ManifestStep, get_manifest_service
 
 logger = logging.getLogger(__name__)
 
@@ -13,7 +14,9 @@ class GO(IPlugin):
         """
         Constructor, prepare the logging subsystem
         """
+        super().__init__()
         self._logger = logging.getLogger(__name__)
+        self.step_name = "GO"
 
     def process(self, conf, output, cmd_conf=None):
         """
@@ -24,5 +27,7 @@ class GO(IPlugin):
         :param cmd_conf: NOT USED
         """
         self._logger.info("[STEP] BEGIN, GO")
-        Downloads(output.prod_dir).exec(conf)
+        manifest_service = get_manifest_service()
+        manifest_step = manifest_service.get_step(self.step_name)
+        manifest_step.resources.append(Downloads(output.prod_dir).exec(conf))
         self._logger.info("[STEP] END, GO")
