42fd0525d7d6af4b2320b1d1df2c059c659e8ea2
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-11-10 11:23:05 +0000

    manifest model refactoring

--- manifest/models.py
+++ manifest/models.py
@@ -1,6 +1,7 @@
 # Manifest file Models
 import typing
 import logging
+from enum import StrEnum
 from modules.common.TimeUtils import get_timestamp_iso_utc_now
 
 # Logging
@@ -18,6 +19,10 @@ class ManifestDocument(object):
         self.created :str = timestamp
         self.modified :str = timestamp
         self.steps :typing.Dict[str, ManifestStep] = dict()
+        self.status_completion :str = ManifestStatus.UNKNOWN
+        self.status_validation :str = ManifestStatus.UNKNOWN
+        self.msg_completion :str = ManifestStatus.UNKNOWN
+        self.msg_validation :str = ManifestStatus.NOT_VALIDATED
 
 # TODO - Step Data model
 class ManifestStep(object):
@@ -26,8 +31,12 @@ class ManifestStep(object):
             timestamp = get_timestamp_iso_utc_now()
         self.created :str = timestamp
         self.modified :str = timestamp
-        self.name :str = "NONAME"
+        self.name :str = ManifestStatus.NO_NAME
         self.resources :list = list()
+        self.status_completion :str = ManifestStatus.UNKNOWN
+        self.status_validation :str = ManifestStatus.UNKNOWN
+        self.msg_completion :str = ManifestStatus.UNKNOWN
+        self.msg_validation :str = ManifestStatus.NOT_VALIDATED
 
 # TODO - Resource data model
 class ManifestResource(object):
@@ -36,7 +45,23 @@ class ManifestResource(object):
             timestamp = get_timestamp_iso_utc_now()
         self.created :str = timestamp
         self.modified :str = timestamp
-        self.source_url :str = "NOT SPECIFIED"
-        self.path_destination :str = "NOT SPECIFIED"
-        self.sha1sum :str = "NOT SPECIFIED"
-        self.md5sum : str = "NOT SPECIFIED"
+        self.source_url :str = ManifestStatus.NOT_SET
+        self.path_destination :str = ManifestStatus.NOT_SET
+        self.sha1sum :str = ManifestStatus.NOT_SET
+        self.md5sum : str = ManifestStatus.NOT_SET
+        self.status_completion :str = ManifestStatus.UNKNOWN
+        self.status_validation :str = ManifestStatus.UNKNOWN
+        self.msg_completion :str = ManifestStatus.UNKNOWN
+        self.msg_validation :str = ManifestStatus.NOT_VALIDATED
+
+class ManifestStatus(StrEnum):
+    ERROR = "ERROR"
+    FAILED = "FAILED"
+    NO_NAME = "NONAME"
+    UNKNOWN = "UNKNOWN"
+    SUCCESS = "SUCCESS"
+    INVALID = "INVALID"
+    NOT_SET = "NOT_SET"
+    NOT_PROVIDED = "NOT_PROVIDED"
+    NOT_VALIDATED = "NOT_VALIDATED"
+
