0e0846c7622dfcfa1df79c63d0ab2ef19892c282
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-11-11 09:57:57 +0000

    report via manifest subsystem for 'reactome' step

--- plugins/Reactome.py
+++ plugins/Reactome.py
@@ -1,6 +1,7 @@
 import logging
 from yapsy.IPlugin import IPlugin
 from modules.common.Downloads import Downloads
+from manifest import ManifestStatus, get_manifest_service
 
 logger = logging.getLogger(__name__)
 
@@ -14,6 +15,7 @@ class Reactome(IPlugin):
         Constructor, prepare the logging subsystem
         """
         self._logger = logging.getLogger(__name__)
+        self.step_name = "Reactome"
 
     def process(self, conf, outputs, cmd_conf=None):
         """
@@ -24,5 +26,9 @@ class Reactome(IPlugin):
         :param cmd_conf: NOT USED
         """
         self._logger.info("[STEP] BEGIN, reactome")
-        Downloads(outputs.prod_dir).exec(conf)
+        manifest_service = get_manifest_service()
+        manifest_step = manifest_service.get_step(self.step_name)
+        manifest_step.resources.extend(Downloads(outputs.prod_dir).exec(conf))
+        manifest_step.status_completion = ManifestStatus.COMPLETED
+        manifest_step.msg_completion = "The step has completed its execution"
         self._logger.info("[STEP] END, reactome")
