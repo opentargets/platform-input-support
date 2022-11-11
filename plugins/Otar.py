e703f93dee6e1dab7175a9d473827fb2718a4318
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-11-11 14:53:14 +0000

    OTAR step reporting via manifest subsystem

--- plugins/Otar.py
+++ plugins/Otar.py
@@ -3,6 +3,7 @@ import logging
 
 from yapsy.IPlugin import IPlugin
 from modules.common import create_folder
+from manifest import ManifestStatus, get_manifest_service
 from modules.common.GoogleSpreadSheet import get_spreadsheet_handler
 
 logger = logging.getLogger(__name__)
@@ -11,13 +12,18 @@ logger = logging.getLogger(__name__)
 This module gathers all data related to OTAR Projects (metadata and mapping)
 """
 
-
 class Otar(IPlugin):
     def __init__(self):
         self._logger = logging.getLogger(__name__)
+        self.step_name = "OTAR"
+
+    @staticmethod
+    def __get_spreadsheet_url(spreadsheet_id: str = ""):
+        return f"https://docs.google.com/spreadsheets/d/{spreadsheet_id}"
 
     def process(self, conf, output, cmd_conf=None):
         self._logger.info("[STEP] BEGIN, otar")
+        manifest_step = get_manifest_service().get_step(self.step_name)
         gcp_credentials = conf.gcp_credentials
         dst_folder = os.path.join(output.prod_dir, conf.gs_output_dir)
         self._logger.debug("Prepare destination folder at '{}'".format(dst_folder))
@@ -33,4 +39,11 @@ class Otar(IPlugin):
                                               sheet.output_format,
                                               gcp_credentials)
             handler.download()
+            download_manifest = get_manifest_service().new_resource()
+            download_manifest.source_url = self.__get_spreadsheet_url(sheet.id_spreadsheet)
+            download_manifest.path_destination = path_dst
+            download_manifest.msg_completion = f"Source URL is the Spreadsheet," \
+                                               f" data downloaded from its worksheet with name '{sheet.worksheet_name}'"
+            download_manifest.status_completion = ManifestStatus.COMPLETED
+            manifest_step.resources.append(download_manifest)
         self._logger.info("[STEP] END, otar")
