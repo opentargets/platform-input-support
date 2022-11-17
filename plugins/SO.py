c2ef5563682d45927a966938ce8ddadc2a749001
Manuel Bernal Llinares <mbdebian@gmail.com>
2022-11-17 15:34:21 +0000

    step level error handling for 'SO'

--- plugins/SO.py
+++ plugins/SO.py
@@ -33,15 +33,31 @@ class SO(IPlugin):
         manifest_step = get_manifest_service().get_step(self.step_name)
         riot = Riot(cmd_conf)
         download_manifest = Downloads.download_staging_http(output.staging_dir, conf.etl)
-        file_ouput_path = os.path.join(output.prod_dir, conf.etl.path)
-        create_folder(file_ouput_path)
-        download_manifest.path_destination = riot.convert_owl_to_jsonld(download_manifest.path_destination,
-                                                                        file_ouput_path,
-                                                                        conf.etl.owl_jq)
-        download_manifest.msg_completion = f"Original converted to JSON and filter with JQ using '{conf.etl.owl_jq}'"
-        download_manifest.status_completion = ManifestStatus.COMPLETED
+        if download_manifest.status_completion == ManifestStatus.COMPLETED:
+            download_manifest.status_completion = ManifestStatus.FAILED
+            file_ouput_path = os.path.join(output.prod_dir, conf.etl.path)
+            create_folder(file_ouput_path)
+            try:
+                download_manifest.path_destination = riot.convert_owl_to_jsonld(download_manifest.path_destination,
+                                                                                file_ouput_path,
+                                                                                conf.etl.owl_jq)
+            except Exception as e:
+                download_manifest.msg_completion = f"FAILED to convert OWL file" \
+                                                   f" '{download_manifest.path_destination}'" \
+                                                   f" to JSON and filter it with JQ filter '{conf.etl.owl_jq}'" \
+                                                   f" due to '{e}'"
+            else:
+                download_manifest.msg_completion = f"Original converted to JSON and filtered with JQ using" \
+                                                   f" '{conf.etl.owl_jq}'"
+                download_manifest.status_completion = ManifestStatus.COMPLETED
         manifest_step.resources.append(download_manifest)
         get_manifest_service().compute_checksums(manifest_step.resources)
-        manifest_step.status_completion = ManifestStatus.COMPLETED
-        manifest_step.msg_completion = "The step has completed its execution"
+        # TODO - Validation
+        if download_manifest.status_completion == ManifestStatus.COMPLETED:
+            manifest_step.status_completion = ManifestStatus.COMPLETED
+            manifest_step.msg_completion = "The step has completed its execution"
+        else:
+            manifest_step.status_completion = ManifestStatus.FAILED
+            manifest_step.msg_completion = download_manifest.msg_completion
+            self._logger.error(manifest_step.msg_completion)
         self._logger.info("[STEP] END, so")
