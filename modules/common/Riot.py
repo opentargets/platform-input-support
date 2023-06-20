c58f40f9ebd3c221fa9e63b6c621a6079d03870f
Ehsan <plato29@gmail.com>
2023-06-20 15:06:48 +0100

    Change subprocess to run Riot and JQ

--- modules/common/Riot.py
+++ modules/common/Riot.py
@@ -1,6 +1,6 @@
-import os
 import errno
 import logging
+import os
 import subprocess
 from modules.common.Utils import Utils
 
@@ -53,30 +53,54 @@ class Riot(object):
         # Set JVM memory limits
         run_env = os.environ.copy()
         run_env["_JAVA_OPTIONS"] = "-Xms4096m -Xmx8192m"
+
         try:
-            with open(path_output, "wb") as json_output, \
-                    subprocess.Popen([self.riot_cmd, "--output", "JSON-LD", owl_file], env=run_env,
-                                     stdout=subprocess.PIPE,
-                                     stderr=subprocess.PIPE) as riot_process, \
-                    subprocess.Popen([self.jq_cmd, "-r", owl_jq], env=run_env,
-                                     stdin=riot_process.stdout,
-                                     stdout=subprocess.PIPE,
-                                     stderr=subprocess.PIPE) as jq_process:
-                json_output.write(jq_process.stdout.read())
-                logger.error(f"RIOT stderr -> '{riot_process.stderr.read()}'")
-                logger.error(f"JQ stderr -> '{jq_process.stderr.read()}'")
-        except EnvironmentError as e:
-            msg_err = "When running RIOT for OWL file '{}', "
-            "with destination path '{}' and JQ filter '{}', "
-            "the following error occurred: '{}'".format(owl_file, path_output, owl_jq, e)
-            logger.error(msg_err)
-            raise RiotException(msg_err)
-        except Exception as e:
-            msg_err = "When running RIOT for OWL file '{}', "
-            "with destination path '{}' and JQ filter '{}', "
-            "the following error occurred: '{}'".format(owl_file, path_output, owl_jq, e)
-            logger.error(msg_err)
-            raise RiotException(msg_err)
+            riot_output = subprocess.run([self.riot_cmd, "--output", "JSON-LD", owl_file], env=run_env, capture_output=True)
+            riot_output.check_returncode()
+            if len(riot_output.stdout) == 0:
+               logger.error(f"Riot command failed on the file {owl_file},  " 
+                             f"Riot output is empty!")
+               raise RiotException()
+               
+            else:
+                                
+                # jq_output = subprocess.run([self.jq_cmd, "-r", owl_jq], input=riot_output.stdout, 
+                #                 stdout=open(path_output, "wb"), stderr=subprocess.PIPE)
+                jq_output = subprocess.run([self.jq_cmd, "-r", owl_jq], input=riot_output.stdout, 
+                                        capture_output= True)
+                # print(f"---------> {jq_output.stdout[0:1000]}")
+                # print(f"---------> {jq_output.stdout.decode('utf-8')[0:1000]}")
+                if len(jq_output.stdout):
+                    with open(path_output, "wb") as json_output:
+                        json_output.write(jq_output.stdout.decode('utf-8'))
+                else:
+                    logger.error(f"JQ filter '{owl_jq}' failed on the Riot output file, " 
+                                f"JQ output is empty!")  
+                    raise Exception()              
+                
+                jq_output.check_returncode()
+
+                logger.info(f"jq_output size: {os.path.getsize(path_output)}")
+                if os.path.getsize(path_output) == 0:
+                    logger.error(f"JQ filter '{owl_jq}' failed on the Riot output file, " 
+                                f"destination file '{path_output}' is empty!")
+                    raise Exception
+                        
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Error in running command {riot_output.args}: {e.stderr.decode('utf-8')} and this code: {e.returncode}")
+            msg = "When running RIOT for OWL file '{}', \
+                    with destination path '{}' and JQ filter '{}', \
+                    the following error occurred: '{}'".format(owl_file, path_output, owl_jq, e)
+            logger.error(f"Error no: {e.returncode}, Error str: {e.stderr.decode('utf-8')}, Msg: {msg}")
+            raise
+        
+        except OSError as e:            
+            msg = "When running RIOT for OWL file '{}', \
+                    with destination path '{}' and JQ filter '{}', \
+                    the following error occurred: '{}'".format(owl_file, path_output, owl_jq, e)
+            logger.error(f"Error no: {e.errno}, Error str: {e.strerror}, Msg: {msg}")
+            raise
+
         return path_output
 
     def convert_owl_to_jsonld(self, owl_file, output_dir, owl_jq):
