9ad07010a4faf084081f5b1f6ab8fd25a2745ee5
James Hayhurst <hayhurst.jd@gmail.com>
2024-02-07 09:46:59 +0000

    ensembl version in config, bump tf provider version

--- launcher/main.tf
+++ launcher/main.tf
@@ -5,11 +5,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.55.0"
+      version = ">= 5.15.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.55.0"
+      version = ">= 5.15.0"
     }
   }
 }
