7a8e9b3199a1d473b49d2c0aa528a7446fe30c77
Javier Ferrer <jferrer@ebi.ac.uk>
2024-05-09 08:57:10 +0100

    Add nogcp makefile target

--- launcher/scripts/pipeline/run.sh
+++ launcher/scripts/pipeline/run.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-config=$(realpath ${OTOPS_PATH_PIS_CONFIG})
-output=$(realpath ${OTOPS_PATH_PIS_OUTPUT_DIR})
-logs=$(realpath ${OTOPS_PATH_PIS_LOGS_DIR})
-creds=$(realpath ${OTOPS_PATH_GCS_CREDENTIALS_FILE})
+config=$(realpath "${OTOPS_PATH_PIS_CONFIG}")
+output=$(realpath "${OTOPS_PATH_PIS_OUTPUT_DIR}")
+logs=$(realpath "${OTOPS_PATH_PIS_LOGS_DIR}")
+creds=$(realpath "${OTOPS_PATH_GCS_CREDENTIALS_FILE}")
 #Removing outermost quotes
 pis_args="${OTOPS_PIS_RUN_ARGS#\"}"
 pis_args="${pis_args%\"}"
+
+gb_arg_line=
+
+params=$(getopt -o n --long nogcp -- "$@")
+eval set -- "$params"
+unset params
+
+case $1 in
+  -n|--nogcp)
+    gb_arg_line=""
+    ;;
+  --)
+    gb_arg_line="-gb ${OTOPS_PATH_GCS_PIS_OUTPUT}"
+    upload_logs=true
+    ;;
+esac
+
 
 run_cmd="docker run \
          -v ${output}:/srv/output \
@@ -17,9 +34,11 @@ run_cmd="docker run \
          -o /srv/output \
          --log-level=DEBUG \
          -gkey /srv/credentials/open-targets-gac.json \
-         -gb ${OTOPS_PATH_GCS_PIS_OUTPUT} \
+         $gb_arg_line \
          $pis_args"
 
 $run_cmd
 
-gsutil -m rsync -r ${logs}/ ${OTOPS_PATH_GCS_PIS_SESSION_LOGS}
+if [ -n "$upload_logs" ]; then
+  gsutil -m rsync -r "${logs}"/ "${OTOPS_PATH_GCS_PIS_SESSION_LOGS}"
+fi
