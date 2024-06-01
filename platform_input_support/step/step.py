61868564941357e47366f870c0ef6343c06da278
Javier Ferrer <jferrer@ebi.ac.uk>
2024-06-01 21:29:00 +0100

    Step repository and go step example

---
+++ platform_input_support/step/step.py
@@ -0,0 +1,17 @@
+from platform_input_support.logger import logger
+
+from platform_input_support.config import config
+from platform_input_support.manifest.services import StepReporter
+
+
+class Step(StepReporter):
+    def __init__(self):
+        self.name = self.__class__.__name__.lower()
+        self.parts = config.steps[self.name]
+
+        super().__init__()
+
+        logger.debug(f'initialized step {self.name}')
+
+    def run(self):
+        pass
