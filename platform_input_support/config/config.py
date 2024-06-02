9d366b837e1253912bf5026f37f05927c34b6ae4
Javier Ferrer <jferrer@ebi.ac.uk>
2024-06-02 23:13:20 +0100

    Config validation

--- platform_input_support/config/config.py
+++ platform_input_support/config/config.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from pathlib import Path
 
 # this module is initialized before the logger, so we must use loguru here
@@ -14,7 +16,7 @@ class Config:
     def __init__(self):
         self.config: ConfigModel
 
-        logger.info('initializing configuration')
+        logger.debug('initializing configuration')
 
         # parse the cli arguments
         cli_parser = ParseCLI()
@@ -27,15 +29,33 @@ class Config:
         yaml_parser.parse()
         yaml_config = ConfigModel.from_dict(yaml_parser.data)
 
-        # merge the config key from both parsers keeping the values from the cli parser
-        yaml_config.settings += cli_settings
+        # merge and validate settings
+        merged_settings = yaml_config.settings + cli_settings
+        self._check_output_path(merged_settings.output_path)
 
-        # set the config attribute
+        # update config
+        yaml_config.settings = merged_settings
         self.config = yaml_config
 
         # print info about the steps found in the config
         step_list = list(self.config.steps.keys())
-        logger.info(f'found configuration for {len(step_list)} steps: {step_list}')
+        logger.debug('configuration initialization finished')
+        logger.info(f'found {len(step_list)} steps: {step_list}')
+
+    def _check_output_path(self, output_path: str) -> None:
+        if Path(output_path).exists():
+            logger.debug(f'output path exists: {output_path}')
+            if not os.access(output_path, os.W_OK):
+                logger.critical(f'output path is not writable: {output_path}')
+                sys.exit(1)
+        if not Path(output_path).is_dir():
+            logger.info(f'output path does not exist, creating: {output_path}')
+
+            try:
+                Path(output_path).mkdir(parents=True, exist_ok=True)
+            except OSError as e:
+                logger.critical(f'error creating output path: {e}')
+                sys.exit(1)
 
 
 c = Config()
