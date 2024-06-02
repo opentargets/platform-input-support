6caa56c0085e24af21239cdf4259033d1c5c7417
Javier Ferrer <jferrer@ebi.ac.uk>
2024-06-02 11:02:51 +0100

    Config models

--- platform_input_support/core.py
+++ platform_input_support/core.py
@@ -1,11 +1,5 @@
 # Custom modules
-from addict import Dict
-from loguru import logger
-
-from platform_input_support.config import config
 from platform_input_support.logger import logger
-from platform_input_support.manifest.services import get_manifest_service
-from platform_input_support.modules.retrieve_resource import RetrieveResource
 
 
 class PISRunnerError(Exception):
@@ -15,25 +9,13 @@ class PISRunnerError(Exception):
 def main():
     logger.info('starting platform input support')
 
-    steps = config.get('steps', {})
-    step_names = ', '.join(list(steps.keys()))
-    logger.info(f'{len(steps)} steps parsed from config: {step_names}')
-
-    # mock stuff here to make things work again - This is temporary
-    yaml_dict = config
-    yaml_dict = Dict(yaml_dict)
-    yaml_dict.manifest = {'file_name': 'manifest.json'}
-    config['output_dir'] = './output'
-    config['force_clean'] = False
-
-    # Resources retriever
-    resources = RetrieveResource(config, yaml_dict)
-    # Session's Manifest
-    manifest_config = yaml_dict
-    manifest_config.update({'output_dir': resources.output_dir_prod})
-    # Initialize the manifest service
-    _ = get_manifest_service(config, manifest_config)
-    resources.run()
+    # # # Resources retriever
+    # resources = RetrieveResource(yaml_dict['settings'], yaml_dict)
+    # # # Session's Manifest
+    # manifest_config = yaml_dict
+    # print(yaml_dict)
+    # manifest_config.update({'output_dir': resources.output_dir_prod})
+    # _ = get_manifest_service(yaml_dict['settings'], manifest_config)
 
 
 if __name__ == '__main__':
