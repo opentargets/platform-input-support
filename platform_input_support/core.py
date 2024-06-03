c0e39145f6ebd11bf08e3ea52a605e575e57173b
Javier Ferrer <jferrer@ebi.ac.uk>
2024-06-03 23:45:55 +0100

    Move old cruft

--- platform_input_support/core.py
+++ platform_input_support/core.py
@@ -25,14 +25,6 @@ def main():
 
     # step_repository.run()
 
-    # # # Resources retriever
-    # resources = RetrieveResource(yaml_dict['settings'], yaml_dict)
-    # # # Session's Manifest
-    # manifest_config = yaml_dict
-    # print(yaml_dict)
-    # manifest_config.update({'output_dir': resources.output_dir_prod})
-    # _ = get_manifest_service(yaml_dict['settings'], manifest_config)
-
 
 if __name__ == '__main__':
     main()
