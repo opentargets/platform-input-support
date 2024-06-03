93e9d17913dbeebfc609fbe7662a1e45d4f4ec1e
Javier Ferrer <jferrer@ebi.ac.uk>
2024-06-03 23:36:46 +0100

    TEMP MANIFEST

---
+++ platform_input_support/manifest/models.py
@@ -0,0 +1,49 @@
+import datetime
+from dataclasses import dataclass, field
+from enum import auto
+
+from strenum import StrEnum
+
+
+class Status(StrEnum):
+    NOT_SET = auto()
+    SUCCESS = auto()
+    FAILED = auto()
+    COMPLETED = auto()
+    NOT_COMPLETED = auto()
+    VALIDATED = auto()
+    VALIDATION_FAILED = auto()
+
+
+@dataclass
+class PartReport:
+    status: Status = Status.NOT_COMPLETED
+    created: datetime.datetime = field(default_factory=datetime.datetime.now)
+    log: list[str] = field(default_factory=list)
+
+
+@dataclass
+class StepReport:
+    name: str | Status = Status.NOT_SET
+    status: Status = Status.NOT_COMPLETED
+    parts: list[PartReport] = field(default_factory=list)
+    created: datetime.datetime = field(default_factory=datetime.datetime.now)
+    modified: datetime.datetime = field(default_factory=datetime.datetime.now)
+    log: list[str] = field(default_factory=list)
+
+
+@dataclass
+class ManifestReport:
+    status: Status = Status.NOT_SET
+    steps: dict[str, StepReport] = field(default_factory=dict)
+    created: datetime.datetime = field(default_factory=datetime.datetime.now)
+    modified: datetime.datetime = field(default_factory=datetime.datetime.now)
+    log: list[str] = field(default_factory=list)
+
+
+@dataclass
+class ResourceReport(PartReport):
+    source_url: str | Status = Status.NOT_SET
+    destination_path: str | Status = Status.NOT_SET
+    checksum_destination: str | Status = Status.NOT_SET
+    checksum_source: str | Status = Status.NOT_SET
