45be28a28556fae340916b44870613d90868adaf
Javier Ferrer <jferrer@ebi.ac.uk>
2024-06-05 09:35:56 +0100

    Remove old logger formatter

--- platform_input_support/logger/logger.py
+++ platform_input_support/logger/logger.py
@@ -1,6 +1,3 @@
-import logging
-import logging.config
-import logging.handlers
 import sys
 from pathlib import Path
 
@@ -9,19 +6,6 @@ from loguru import logger
 from platform_input_support.config.models import SettingsModel
 
 __all__ = ['logger']
-
-
-class CustomFormatter(logging.Formatter):
-    def format(self, record):
-        saved_name = record.name
-        parts = record.name.split('.')
-        if len(parts) and parts[0] == 'platform_input_support':
-            parts = parts[1:]
-
-        record.name = '.'.join(parts)
-        result = super().format(record)
-        record.name = saved_name
-        return result
 
 
 class Logger:
