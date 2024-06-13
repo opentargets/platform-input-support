24e83ef2d328ddc008b4b6e34937dd4e82edd3a9
Javier Ferrer <jferrer@ebi.ac.uk>
2024-06-13 01:12:56 +0100

    Parallelize actions

--- platform_input_support/step/step.py
+++ platform_input_support/step/step.py
@@ -1,6 +1,13 @@
+from multiprocessing import Pool
+
+from loguru import logger
+
 from platform_input_support.action import action_repository
 from platform_input_support.config.models import ActionMapping, StepMapping
 from platform_input_support.manifest.manifest import StepReporter
+
+PARALLEL_STEP_COUNT = 5
+PREPROCESS_ACTIONS = ['explode', 'get_file_list']
 
 
 class Step(StepReporter):
@@ -10,16 +17,31 @@ class Step(StepReporter):
 
         super().__init__(name)
 
-    def run(self):
-        self.start_step()
+    def _preprocess(self):
+        for i, action_mapping in enumerate(self.actions):
+            if action_mapping.get_action_effective_name() in PREPROCESS_ACTIONS:
+                self._run_action(action_mapping)
+                del self.actions[i]
 
-        for action_mapping in self.actions:
-            action = action_repository.actions[action_mapping.name](action_mapping.config)
-            self.add_action(action._report)
+    def _run_action(self, action_mapping: ActionMapping):
+        action_type = action_repository.actions[action_mapping.get_action_effective_name()]
+        action = action_type(action_mapping)
+        self.add_action(action._report)
 
+        with logger.contextualize(action=action.name):
             try:
                 action.run()
             except Exception as e:
                 self.fail_step(e)
+
+    def run(self):
+        self.start_step()
+
+        logger.info('running preprocessing actions')
+        self._preprocess()
+
+        logger.info('running main actions')
+        with Pool(PARALLEL_STEP_COUNT) as p:
+            p.map(self._run_action, self.actions)
 
         self.complete_step()
