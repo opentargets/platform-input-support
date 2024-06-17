7ecee7c735096b31be7358ac5c6c16405d4129da
Javier Ferrer <jferrer@ebi.ac.uk>
2024-06-17 18:31:58 +0100

    Improve error handling

--- platform_input_support/util/errors.py
+++ platform_input_support/util/errors.py
@@ -3,6 +3,19 @@ import sys
 from loguru import logger
 
 
+class NotFoundError(Exception):
+    def __init__(self, resource: str):
+        message = f'{resource} not found'
+        logger.error(message)
+        super().__init__(message)
+
+
+class HelperError(Exception):
+    def __init__(self, message: str):
+        logger.error(message)
+        super().__init__(message)
+
+
 def log_and_raise(error: Exception) -> None:
     logger.opt(exception=sys.exc_info()).error(error)
     raise error
