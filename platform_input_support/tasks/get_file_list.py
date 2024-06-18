6bbe90966352862401cd3f24d0d4323571cd72dc
Javier Ferrer <jferrer@ebi.ac.uk>
2024-06-18 22:05:49 +0100

    Clean up

--- platform_input_support/tasks/get_file_list.py
+++ platform_input_support/tasks/get_file_list.py
@@ -21,15 +21,16 @@ class GetFileList(Task):
 
     @report_to_manifest
     def run(self):
+        source, pattern = self.config.source, self.config.pattern
         file_list: list[str] = []
 
-        if self.config.pattern.startswith('!'):
-            file_list = google_helper.list(self.config.source, exclude=self.config.pattern[1:])
+        if pattern.startswith('!'):
+            file_list = google_helper.list(source, exclude=pattern[1:])
         else:
-            file_list = google_helper.list(self.config.source, include=self.config.pattern)
+            file_list = google_helper.list(source, include=pattern)
 
         if len(file_list):
             scratchpad.store(self.config.scratchpad_key, file_list)
-            return f'{len(file_list)} files with pattern {self.config.pattern} found in {self.config.source}'
+            return f'{len(file_list)} files with pattern {pattern} found in {source}'
         else:
-            raise ValueError(f'no files found in {self.config.source} with pattern {self.config.pattern}')
+            raise ValueError(f'no files found in {source} with pattern {pattern}')
