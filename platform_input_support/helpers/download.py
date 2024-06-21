771052e8bc2ffe842457bb20bd3856ad17834e3a
Javier Ferrer <jferrer@ebi.ac.uk>
2024-06-21 12:16:26 +0100

    Ensure content is decoded when downloading

--- platform_input_support/helpers/download.py
+++ platform_input_support/helpers/download.py
@@ -1,3 +1,4 @@
+import functools
 import shutil
 from pathlib import Path
 
@@ -8,21 +9,20 @@ from urllib3 import Retry
 
 from platform_input_support.helpers import google_helper
 from platform_input_support.util.errors import DownloadError
-from platform_input_support.util.misc import check_dir, get_full_path
+from platform_input_support.util.fs import check_dir, get_full_path
 
 # we are going to download big files, better to use a big chunk size
 CHUNK_SIZE = 1024 * 1024 * 10
 
 
-def download(src: str, dst: Path | None = None) -> Path:
-    if dst is None:
-        dst = Path(src.split('/')[-1].split('?')[0])
+def download(src: str, dst: Path) -> Path:
     dst = get_full_path(dst)
-    logger.info(f'downloading `{src}` to `{dst}`')
+    logger.info(f'preparing to download `{src}` to `{dst}`')
     check_dir(dst)
 
     # download from google sheets
     if src.startswith('https://docs.google.com/spreadsheets/d'):
+        logger.info('starting google sheets download')
         s = google_helper.get_session()
         _download(src, dst, s)
 
@@ -31,6 +31,8 @@ def download(src: str, dst: Path | None = None) -> Path:
 
         # download from http/https
         if proto in ['http', 'https']:
+            logger.info('starting http(s) download')
+
             s = requests.Session()
             retries = Retry(
                 total=5,
@@ -38,12 +40,14 @@ def download(src: str, dst: Path | None = None) -> Path:
                 status_forcelist=[500, 502, 503, 504],
                 allowed_methods={'GET'},
             )
+
             s.mount('http://', requests.adapters.HTTPAdapter(max_retries=retries))
             s.mount('https://', requests.adapters.HTTPAdapter(max_retries=retries))
             _download(src, dst, s)
 
         # download from google storage
         elif proto == 'gs':
+            logger.info('starting google storage download')
             google_helper.download(src, dst)
 
     return dst
@@ -53,8 +57,11 @@ def _download(src: str, dst: Path, s: requests.Session):
     r = s.get(src, stream=True, timeout=(10, None))
     r.raise_for_status()
 
+    # ensure we decode the content
+    r.raw.read = functools.partial(r.raw.read, decode_content=True)
+
     with open(dst, 'wb') as f:
         try:
-            shutil.copyfileobj(r.raw, f, CHUNK_SIZE)
+            shutil.copyfileobj(r.raw, f)
         except Exception as e:
             raise DownloadError(src, e)
