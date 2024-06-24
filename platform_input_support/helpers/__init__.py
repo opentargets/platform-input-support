fe6e35d1f3869c4817740d666de19ec062277696
Javier Ferrer <jferrer@ebi.ac.uk>
2024-06-24 09:20:14 +0100

    Improve google helper instantiation

--- platform_input_support/helpers/__init__.py
+++ platform_input_support/helpers/__init__.py
@@ -1,3 +1,27 @@
 from platform_input_support.helpers.google import GoogleHelper
+from platform_input_support.util.errors import PISError
 
-google_helper = GoogleHelper()
+_google_helper: GoogleHelper | None = None
+
+
+def init_google_helper():
+    global _google_helper  # noqa: PLW0603
+    if _google_helper is None:
+        _google_helper = GoogleHelper()
+
+
+def google_helper():
+    """Return the Google helper.
+
+    The Google helper must be initialized explicitly by calling the
+    `_init_google_helper` function before trying to access it. If it is not
+    initialized, a PISError is raised.
+
+    Returns:
+        GoogleHelper: The Google helper.
+    """
+    if _google_helper is None:
+        raise PISError('Google helper not initialized')
+
+    assert _google_helper is not None
+    return _google_helper
