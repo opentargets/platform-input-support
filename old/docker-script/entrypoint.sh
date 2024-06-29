45d6cd52f75f43887b8d973d862a312442054f43
Javier Ferrer <jferrer@ebi.ac.uk>
2024-06-29 16:17:35 +0100

    Remove old stuff

--- old/docker-script/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "[GCP] Activate the provided service account"
-gcloud auth activate-service-account --key-file=/srv/credentials/open-targets-gac.json
-
-echo "[PIS] Running Platform input support with parameters $*"
-poetry run python3 platform-input-support.py "$@"
-
-exit $!
--- old/launcher/outputs.tf
+++
@@ -1,9 +0,0 @@
-// POS VM name
-output "pisvm" {
-  value = {
-    name = google_compute_instance.pisvm.name
-    zone = google_compute_instance.pisvm.zone
-    username = local.pisvm_remote_user_name
-  }
-  description = "PIS VM information"
-}
